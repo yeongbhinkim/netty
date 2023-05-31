8aed2e410bc4f892c2756d522ea51d8474bbc6d4
fkdle <abc282v@naver.com>
2023-05-31 18:34:36 +0900

    2 commit

--- src/main/java/org/example/netty/ClientHandler.java
+++ src/main/java/org/example/netty/ClientHandler.java
@@ -9,7 +9,10 @@ import org.example.rbpClient.service.RbpClientService;
  * 클라이언트는 서버로부터 문자열을 받아 channelRead0() 메소드를 호출한다.
  * 이 메소드는 결국 받은 문자열을 화면에 출력한다.
  *
- * ChannelHandler 는 SocketChannel에서 얽혀진 데이터나 SocketChannel로 쓰여질 데이터를 다루게됩니다
+ * 각각의 SocketChannel은 ChannelPipeline을 가지고 있다 채널파이프라인은 ChannelHandler
+ * 인스턴스의 리스트다 EventLoop이 데이터를 SocketChannel에서 읽으면 데이터는 파이프라인에 있는 첫번째 채널핸들러에게 넘겨진다
+ * 첫 번째 핸들러는 넘겨받은 데이터를 처리하고 필요한 경우 파이프라인의 다음 핸들러로 데이터를 넘길 수 있다
+ * 데이터를 SocketChannel로 쓰는 경우도 마찬가지로 ChannelPipeline을 타게 되며 핸들러들은 거친다음 SocketChannel로 쓰여지게됩니다
  */
 public class ClientHandler extends SimpleChannelInboundHandler {
 
